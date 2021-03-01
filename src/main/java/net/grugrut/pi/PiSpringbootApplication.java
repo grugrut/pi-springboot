e58ae18a3c14f163b17732b92490117a87efae35
grugut <grugruglut+github@gmail.com>
2021-03-01 22:58:40 +0900

    Initial commit.

---
+++ src/main/java/net/grugrut/pi/PiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package net.grugrut.pi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/grugrut/pi/PiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package net.grugrut.pi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
